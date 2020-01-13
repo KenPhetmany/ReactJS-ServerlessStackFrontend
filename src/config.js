ed3f5b80190e563da0cc2245e7cab5b3add64294
KenPhetmany <kphetmany@student.uts.edu.au>
2020-01-13 12:30:34 +1100

    UPDATE: setting up React

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "ken-notes-apps-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://90m0axxqf9.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_S1lAv8hWW",
+        APP_CLIENT_ID: "2en1j7fqfj2gt9oobqstclo7uj",
+        IDENTITY_POOL_ID: "us-east-2:e1e5894a-3e53-4f09-8148-db1ad1a22fa9"
+    }
+};
