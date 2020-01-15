3da89f7c2379729dc83ae1de7ac2a58e029916dc
KenPhetmany <kphetmany@student.uts.edu.au>
2020-01-15 14:38:04 +1100

    UPDATE: Adding functions

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home/Home";
 import NotFound from "./containers/NotFound/NotFound";
+import Login from "./containers/Login/Login";
+import Signup from "./containers/Signup/Signup"
+import NewNote from "./containers/NewNote/NewNote"
+import ResetPassword from "./containers/ResetPassword/ResetPassword"
 
-export default function Routes() {
+
+export default function Routes({ appProps }) {
     return (
         <Switch>
-            <Route path="/" exact component={Home} />
+            <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+            <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+            <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+            <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+            <AppliedRoute path="/login/reset"
+                exact
+                component={ResetPassword}
+                appProps={appProps}
+            />
             { /* Finally, catch all unmatched routes */}
             <Route component={NotFound} />
         </Switch>
